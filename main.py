d7747048fe0486ca9310bb495a6afc3e5719360e
Renat <70426913+renat2006@users.noreply.github.com>
2023-05-06 16:54:26 +0300

    Initial

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import os
 import re
 import time
 from datetime import datetime
-
-
-
+import psycopg2
 import openai
 from dotenv import load_dotenv
-
-
+from psycopg2 import sql
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
 
-def createPostcontentName():
-    prompt = f"Придумай необычную тему для поста"
+
+def create_question():
+    prompt = f"Придумай вопрос для игры Что? Где? Когда?. Также должен быть ответ и две подсказки. Данные должны быть в формате JSON, где ключи: question, answer, hint1, hint2"
 
     print(prompt)
-    messages = [{"role": "system", "content": "Ты креативный автор блога про здоровый образ жизни"},
+    messages = [{"role": "system", "content": "Ты автор вопросов для игры Что? Где? Когда?"},
                 {"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -25,9 +23,33 @@ def createPostcontentName():
 
     )
 
-    theme_ = response.choices[0].message.content
-    print(theme_)
-    if '.' in theme_:
-        theme_ = theme_.split('.')[0]
-    print(theme_)
-    return theme_
+    data = response.choices[0].message.content
+    print(data)
+
+    return data
+
+def connect_to_db():
+
+
+    conn = psycopg2.connect("""
+        host=rc1b-1dkhcvps79tr5wu2.mdb.yandexcloud.net
+        port=6432
+        dbname=questions
+        user=user1
+        password=Taner2320
+        target_session_attrs=read-write
+    """)
+
+    q = conn.cursor()
+    q.execute('INSERT INTO questions(question, answer, hint1, hint2) VALUES();')
+
+    conn.autocommit = True
+    values = [
+        ()
+    ]
+    insert = sql.SQL('INSERT INTO questions (question, answer, hint1, hint2) VALUES {}').format(
+        sql.SQL(',').join(map(sql.Literal, values))
+    )
+
+    conn.close()
+
