e89e238959c9e734968a66ccdb0c0602098f7aa5
Renat <testinggame778@gmail.com>
2023-05-07 12:47:20 +0300

    v 1.0.3

--- main.py
+++ main.py
@@ -11,11 +11,18 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
 
 
+def check_string(string_to_check):
+    if re.match(r'^[А-Яа-я ]+$', string_to_check) and len(string_to_check.split()) <= 2:
+        return True
+    else:
+        return False
+
+
 def create_question():
-    prompt = f"Придумай вопрос для игры Что? Где? Когда?. Также должен быть ответ и две подсказки. Данные должны быть в формате JSON, где ключи: question, answer, hint1, hint2"
+    prompt = f"Придумай вопрос для викторины. Также должен быть ответ и две подсказки. Ответ должно быть легко произнести. Данные должны быть в формате JSON, где ключи: question, answer, hint1, hint2"
 
     print(prompt)
-    messages = [{"role": "system", "content": "Ты автор вопросов для игры Что? Где? Когда?"},
+    messages = [{"role": "system", "content": "Ты автор вопросов для викторины "},
                 {"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -26,11 +33,10 @@ def create_question():
     data = response.choices[0].message.content
     print(data)
 
-    return data
-
-def connect_to_db():
+    return eval(data)
 
 
+def insert_to_db(data):
     conn = psycopg2.connect("""
         host=rc1b-1dkhcvps79tr5wu2.mdb.yandexcloud.net
         port=6432
@@ -40,16 +46,27 @@ def connect_to_db():
         target_session_attrs=read-write
     """)
 
-    q = conn.cursor()
-    q.execute('INSERT INTO questions(question, answer, hint1, hint2) VALUES();')
+    cur = conn.cursor()
 
-    conn.autocommit = True
-    values = [
-        ()
-    ]
-    insert = sql.SQL('INSERT INTO questions (question, answer, hint1, hint2) VALUES {}').format(
-        sql.SQL(',').join(map(sql.Literal, values))
-    )
+    question = data["question"]
+    answer = data["answer"]
+    hint1 = data["hint1"]
+    hint2 = data["hint2"]
 
+    cur.execute("INSERT INTO chgk_questions (question, answer, hint1, hint2) VALUES (%s, %s, %s, %s)",
+                (question, answer, hint1, hint2))
+
+    conn.commit()
+    cur.close()
     conn.close()
+    print("ok")
 
+
+for i in range(20):
+
+    q_data = create_question()
+    if not check_string(q_data["answer"]):
+        continue
+    insert_to_db(q_data)
+    time.sleep(31)
+    print("__________________")
